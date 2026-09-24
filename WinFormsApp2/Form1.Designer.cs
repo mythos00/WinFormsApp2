1866fd1b856165cf41c60898100afe92cbee0e4c
user <user@MYTHOSS>
2026-09-24 21:44:50 +0400

    Add project files.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            comboBox1 = new ComboBox();
+            label5 = new Label();
+            label6 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(12, 42);
+            label1.Margin = new Padding(6, 0, 6, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(120, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Calculator";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(15, 112);
+            label2.Margin = new Padding(6, 0, 6, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 23);
+            label2.TabIndex = 1;
+            label2.Text = "NUM 1";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(15, 138);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(234, 31);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "0";
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.KeyPress += textBox1_KeyPress;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(15, 217);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(234, 31);
+            textBox2.TabIndex = 4;
+            textBox2.Text = "0";
+            textBox2.TextChanged += textBox2_TextChanged;
+            textBox2.KeyPress += textBox2_KeyPress;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(12, 191);
+            label3.Margin = new Padding(6, 0, 6, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 23);
+            label3.TabIndex = 3;
+            label3.Text = "NUM 2";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(12, 271);
+            label4.Margin = new Padding(6, 0, 6, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(117, 23);
+            label4.TabIndex = 5;
+            label4.Text = "Command";
+            label4.Click += label4_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "+", "-", "*", "/" });
+            comboBox1.Location = new Point(15, 307);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(234, 31);
+            comboBox1.TabIndex = 6;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(15, 369);
+            label5.Margin = new Padding(6, 0, 6, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(106, 23);
+            label5.TabIndex = 7;
+            label5.Text = "Answer :";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(143, 369);
+            label6.Margin = new Padding(6, 0, 6, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(24, 23);
+            label6.TabIndex = 8;
+            label6.Text = "0";
+            label6.Click += label6_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(12, 413);
+            button1.Name = "button1";
+            button1.Size = new Size(237, 32);
+            button1.TabIndex = 9;
+            button1.Text = "Result";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(12, 467);
+            button2.Name = "button2";
+            button2.Size = new Size(237, 32);
+            button2.TabIndex = 10;
+            button2.Text = "clear";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SteelBlue;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(316, 534);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(comboBox1);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            Font = new Font("Verdana", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(6, 5, 6, 5);
+            Name = "Form1";
+            Text = "Calculator";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+        public ComboBox comboBox1;
+        private Label label5;
+        private Label label6;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,182 @@
+﻿namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1;
+            double num2;
+
+            // 1-ci ədədi yoxlayırıq
+            if (!double.TryParse(textBox1.Text, out num1))
+            {
+                MessageBox.Show("NUM 1 düzgün daxil edilməyib!");
+                return;
+            }
+
+            // 2-ci ədədi yoxlayırıq
+            if (!double.TryParse(textBox2.Text, out num2))
+            {
+                MessageBox.Show("NUM 2 düzgün daxil edilməyib!");
+                return;
+            }
+
+            // Əməliyyat seçilib?
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Əməliyyat seçin!");
+                return;
+            }
+
+            string command = comboBox1.SelectedItem.ToString();
+
+            double result = 0;
+
+            switch (command)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+
+                case "-":
+                    result = num1 - num2;
+                    break;
+
+                case "*":
+                    result = num1 * num2;
+                    break;
+
+                case "/":
+                    if (num2 == 0)
+                    {
+                        MessageBox.Show("0-a bölmək olmaz!");
+                        return;
+                    }
+
+                    result = num1 / num2;
+                    break;
+            }
+
+            // Nəticəni label6-da göstər
+            label6.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Clear();
+            textBox2.Clear();
+
+            comboBox1.SelectedIndex = -1;
+
+            label6.Text = "0";
+        
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                return;
+            }
+
+            if (char.IsControl(e.KeyChar))
+            {
+                return;
+            }
+
+            if (e.KeyChar == '-' && textBox1.SelectionStart == 0 && !textBox1.Text.Contains("-"))
+            {
+                return;
+            }
+
+            if ((e.KeyChar == '.' || e.KeyChar == ',') &&
+                !textBox1.Text.Contains(".") &&
+                !textBox1.Text.Contains(","))
+            {
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                return;
+            }
+
+            if (char.IsControl(e.KeyChar))
+            {
+                return;
+            }
+
+            if (e.KeyChar == '-' && textBox2.SelectionStart == 0 && !textBox2.Text.Contains("-"))
+            {
+                return;
+            }
+
+            if ((e.KeyChar == '.' || e.KeyChar == ',') &&
+                !textBox2.Text.Contains(".") &&
+                !textBox2.Text.Contains(","))
+            {
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
